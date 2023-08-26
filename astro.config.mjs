be659ee7d6e38fb4eefdcf56305204823e761b79
moeenn <moeen.v8@gmail.com>
2023-08-26 12:12:44 +0500

    optimizations and deploy config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,11 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs()],
+  site: "https://moeenn.github.io",
+  compressHTML: true,
+  output: "static",
+  build: {
+    assets: "assets",
+  }
 });
