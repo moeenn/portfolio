07457c6cf36591d76949ab17e5675cb1fa024926
moeenn <moeen.v8@gmail.com>
2025-11-16 17:43:18 +0500

    added new projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://moeenn.github.io",
-  compressHTML: true,
-  output: "static",
-  build: {
-    assets: "assets",
-  },
-  server: {
-    port: 3000,
-  }
+	integrations: [tailwind()],
+	site: "https://moeenn.github.io",
+	compressHTML: true,
+	output: "static",
+	build: {
+		assets: "assets",
+	},
+	server: {
+		port: 3000,
+	}
 });
