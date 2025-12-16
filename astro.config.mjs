47601d3207310f771dcb6d3ef15c13d323f32aa8
moeenn <moeen.v8@gmail.com>
2025-12-16 23:52:46 +0500

    updated build configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    output: "static",
+    build: {
+        assets: "assets"
+    },
     vite: {
         plugins: [tailwindcss()],
     },
     server: {
         port: 3000,
-    }
+    },
 });  
