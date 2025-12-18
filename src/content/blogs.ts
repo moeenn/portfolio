19cccb451d2f0b608766361d017f22f56d132561
moeenn <moeen.v8@gmail.com>
2025-12-18 21:48:46 +0500

    add blogs

--- src/content/blogs.ts
+++ src/content/blogs.ts
@@ -12,10 +12,12 @@ const Categories: Record<string, Category> = {
 
 const Tags: Record<string, Tag> = {
 	largeNumbers: new Tag("Large numbers"),
-	node: new Category("Node"),
-	npm: new Category("NPM"),
-	ts: new Category("Typescript"),
-	jsdoc: new Category("JSDoc"),
+	node: new Tag("Node"),
+	npm: new Tag("NPM"),
+	ts: new Tag("Typescript"),
+	jsdoc: new Tag("JSDoc"),
+	express: new Tag("Express"),
+	jwt: new Tag("JWT"),
 };
 
 export class BlogPost {
@@ -54,6 +56,20 @@ export const BlogPosts: BlogPost[] = [
 		id: "node-memory-leaks",
 		title: "Detect memory leaks in NodeJS applications",
 		desc: "Bad code is not only hard to maintain, it can also be hard on the running it. This articles gives a general idea of how to detect memory leaks in a NodeJs/Express application.",
+		category: Categories.js,
+		tags: [Tags.node, Tags.express],
+	},
+	{
+		id: "node-jwt",
+		title: "JWT in Node",
+		desc: "JWTs are a great alternative to sessions and cookies. This article walks through usage of JWTs in NodeJS.",
+		category: Categories.js,
+		tags: [Tags.node, Tags.jwt, Tags.jsdoc],
+	},
+	{
+		id: "pm2-clustering",
+		title: "Clustering and PM2 in Node",
+		desc: "Given the single-threaded nature of Node, clustering is extremely important to ensure resiliance and proper utilization of hardware. This articles walks through some examples.",
 		category: Categories.js,
 		tags: [Tags.node],
 	},
