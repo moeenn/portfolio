e2f84d3024b0af3bf5ea164182765d3810c872ae
moeenn <moeen.v8@gmail.com>
2025-12-18 17:57:28 +0500

    added meta inf

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const button = document.querySelector("[data-navbutton")
-  console.assert(button != null)
+	const button = document.querySelector("[data-navbutton");
+	console.assert(button != null);
 
-  const mobileMenu = document.querySelector("[data-mobilemenu]")
-  console.assert(mobileMenu != null)
+	const mobileMenu = document.querySelector("[data-mobilemenu]");
+	console.assert(mobileMenu != null);
 
-  button?.addEventListener("click", () => {
-    button.classList.toggle("bg-slate-200")
-    mobileMenu?.classList.toggle("hidden")
-  }) 
-})
+	button?.addEventListener("click", () => {
+		button.classList.toggle("bg-gray-200");
+		mobileMenu?.classList.toggle("hidden");
+	});
+});
